b0bd43e7b9affc60cf029f776fdc7801a6e1c0a6
KL <noleafet@gmail.com>
2026-06-18 03:27:52 +0800

    merge: app entry point

--- backend/src/main/java/com/leafnote/openbook/OpenBookApplication.java
+++ backend/src/main/java/com/leafnote/openbook/OpenBookApplication.java
@@ -2,9 +2,17 @@ package com.leafnote.openbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OpenBookApplication {
+public class OpenBookApplication extends SpringBootServletInitializer {
+
+	//For deploying to an external server (WAR)
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OpenBookApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenBookApplication.class, args);
