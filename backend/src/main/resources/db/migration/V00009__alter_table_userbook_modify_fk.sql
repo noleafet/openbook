5fb4f20ce48ebd5a87ba8b580e7775957fa32024
KL <noleafet@gmail.com>
2026-08-02 02:37:07 +0800

    KAN-25 updated sql syntaxes for generation of id, alter column, alter constraints

--- backend/src/main/resources/db/migration/V00009__alter_table_userbook_modify_fk.sql
+++ backend/src/main/resources/db/migration/V00009__alter_table_userbook_modify_fk.sql
@@ -1,8 +1,8 @@
 -- 1. Remove the old behavior
-ALTER TABLE userbook DROP FOREIGN KEY userbook_ibfk_2;
+ALTER TABLE userbook DROP CONSTRAINT userbook_book_id_key;
 
 -- 2. Apply the new behavior
 ALTER TABLE userbook 
-ADD CONSTRAINT userbook_ibfk_2 
+ADD CONSTRAINT userbook_book_id_key 
 FOREIGN KEY (book_id) REFERENCES book (id) 
 ON DELETE CASCADE;
