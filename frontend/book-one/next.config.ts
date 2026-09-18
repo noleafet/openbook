f12eab05bf02a5e3a77affe8b060afb2942a22d8
KL <noleafet@gmail.com>
2026-09-18 22:21:37 +0800

    KAN-46 config for log and landing page updated

--- frontend/book-one/next.config.ts
+++ frontend/book-one/next.config.ts
@@ -7,6 +7,20 @@ const nextConfig: NextConfig = {
       fullUrl: true, // Logs the full URL of server-side fetch requests
     },
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production' 
+      ? { exclude: ['error', 'warn'] } 
+      : false,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/book-one', // Change this to your desired path
+        permanent: true,       // Use true for 301 permanent redirect, false for 302 temporary
+      },
+    ];
+  },
 };
 
 export default nextConfig;
